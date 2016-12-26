bc56e77faf3e5740e0c04b68d0ffbb0b41dca8d0
文优 <thisfin@gmail.com>
2016-12-26 10:33:50 +0800

    Format

--- brewshow.pl
+++ brewshow.pl
@@ -9,10 +9,10 @@ for run('brew', 'list', :out).out.words -> $moduleName {
 	my %valueHash;
     for run('brew', 'info', $moduleName, :out).out.lines -> $line {
         given $line {
-        	for %mappingHash.kv -> $mappingName, $regex {
-        		when /$regex/ {
-        			%valueHash.push($mappingName => ~$/.postmatch.subst(/\s/, '', :g).split(/\,/));
-        		}
+            for %mappingHash.kv -> $mappingName, $regex {
+                when /$regex/ {
+                    %valueHash.push($mappingName => ~$/.postmatch.subst(/\s/, '', :g).split(/\,/));
+                }
             }
         }
     }
